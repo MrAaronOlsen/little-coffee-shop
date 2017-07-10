3400bfd97942a059b4e4b9b75e80215584d31ba4
Aaron Olsen <mraaronolsen@gmail.com>
2017-07-10 10:40:53 -0600

    Access to owner gihub repos

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,40 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'users#show'
 
-  resources :users, except: :delete
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  put '/users/:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
 
-  resources :items, only: [:index, :show]
-  resources :categories, only: [:show]
-  resources :orders, except: %i[update edit destroy]
+  get '/items', to: 'items#index', as: 'items'
+  get '/items/:id', to: 'items#show', as: 'item'
+  get '/categories/:id', to: 'categories#show', as: 'category'
 
-  namespace :admin do
-    get '/dashboard', to: 'users#index'
-    resources :users, only: [:show, :index]
-    resources :orders, only: %i[index show update edit]
-    resources :items
-  end
+  get '/orders', to: 'orders#index', as: 'orders'
+  post '/orders', to: 'orders#create'
+  get '/orders/new', to: 'orders#new', as: 'new_order'
+  get '/orders/:id', to: 'orders#show', as: 'order'
+
+  get 'admin/dashboard', to: 'admin/users#index', as: 'admin_dashboard'
+  get 'admin/users', to: 'admin/users#index', as: 'admin_users'
+  get 'admin/user/:id', to: 'admin/users#show', as: 'admin_user'
+
+  get 'admin/orders', to: 'admin/orders#index', as: 'admin_orders'
+  get 'admin/orders/:id/edit', to: 'admin/orders#edit', as: 'admin_edit_order'
+  get 'admin/orders/:id', to: 'admin/orders#show', as: 'admin_order'
+  put 'admin/orders/:id', to: 'admin/orders#update'
+  patch 'admin/orders/:id', to: 'admin/orders#update'
+
+  get 'admin/items', to: 'admin/items#index', as: 'admin_items'
+  post 'admin/items', to: 'admin/items#create'
+  get 'admin/items/new', to: 'admin/items#new', as: 'new_admin_item'
+  get 'admin/items/:id/edit', to: 'admin/items#edit', as: 'edit_admin_item'
+  get 'admin/items/:id', to: 'admin/items#show', as: 'admin_item'
+  put 'admin/items/:id', to: 'admin/items#update'
+  patch 'admin/items/:id', to: 'admin/items#update'
 
   get '/cart', to: 'carts#show'
   post '/cart', to: 'carts#create'
